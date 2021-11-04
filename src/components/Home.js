af1caf9048fab8eb665366b37105d61f84625531
Shebzzzzz sayed <shehbazsayed33@gmail.com>
2021-11-04 15:56:02 +0530

    completed

---
+++ src/components/Home.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+    return (
+        <div>
+           <Container>
+               <Section 
+                title="Model Y"
+                description="Order online for touchless delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+               />
+               <Section 
+                title="Model X"
+                description="Order online for touchless delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+               />
+               <Section 
+                title="Model S"
+                description="Order online for touchless delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+               />
+               <Section 
+                title="Model 3"
+                description="Order online for touchless delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+               />
+                <Section 
+                title="Lowest Cost Solar Panels"
+                description="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn more"
+               />
+               <Section 
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+                rightBtnText=""
+               />
+           </Container>
+        </div>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height:100vh;
+`
